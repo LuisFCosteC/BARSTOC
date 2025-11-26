6b18d3540726ae67aea5d647b3622e4c8b4f9cc3
Luis F. Coste C. <luisfcostec@gmail.com>
2025-11-25 20:21:37 -0500

    ✨ feat: I have worked on the logic of the BARSTOC_DAL layer.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  standalone: false,
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'BARSTOC-app';
+  protected title = 'frontend';
 }
